df130d77c4cd5dc397210b40e1d213915f6755b1
brintasabit <brintasabit@gmail.com>
2019-09-09 20:18:50 +0600

    100 times input

--- Assignment1_bitm/Assignment1_bitm/Program.cs
+++ Assignment1_bitm/Assignment1_bitm/Program.cs
@@ -6,8 +6,40 @@ using System.Threading.Tasks;
 
 namespace Assignment1_bitm
 {
+    class info
+    {
+        public void infodata()
+        {
+            string[] data = new string[100];
+
+            Console.WriteLine("Enter your name");
+            data[0] = Console.ReadLine();
+            Console.WriteLine("Enter your Age");
+            data[1] = Console.ReadLine();
+            Console.WriteLine("Enter your Address");
+            data[2] = Console.ReadLine();
+            Console.WriteLine("Enter your Contact");
+            data[3] = Console.ReadLine();
+            // Console.WriteLine(data[0] + data[1] +data[2] +data[3]);
+            Console.WriteLine();
+            Console.WriteLine("Name: " + data[0]);
+            Console.WriteLine("Age: " + data[1]);
+            Console.WriteLine("Address: " + data[2]);
+            Console.WriteLine("Contact: " + data[3]);
+            Console.WriteLine();
+
+
+
+           // Console.ReadKey();
+        }
+    
+    }
+
+
     class Program
     {
+        
+       
         static void Main(string[] args)
         {
           /*  string name;
@@ -32,28 +64,12 @@ namespace Assignment1_bitm
             Console.WriteLine("Age: " +age);
             Console.WriteLine("Address: " +address);
             Console.WriteLine("Contact: " +contact);*/
-
-            string[] data = new string[100];
             
-                Console.WriteLine("Enter your name");
-                data[0] = Console.ReadLine();
-                Console.WriteLine("Enter your Age");
-                data[1] = Console.ReadLine();
-                Console.WriteLine("Enter your Address");
-                data[2] = Console.ReadLine();
-                Console.WriteLine("Enter your Contact");
-                data[3] = Console.ReadLine();
-                // Console.WriteLine(data[0] + data[1] +data[2] +data[3]);
-                Console.WriteLine();
-                Console.WriteLine("Name: " + data[0]);
-                Console.WriteLine("Age: " + data[1]);
-                Console.WriteLine("Address: " + data[2]);
-                Console.WriteLine("Contact: " + data[3]);
-
-
-
-                Console.ReadKey();
-            
+                info f = new info();
+                for (int i = 1; i <= 100; i++)
+                {
+                    f.infodata();
+                }
 
         }
     }
